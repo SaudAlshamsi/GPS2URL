72447ee34010a627c55cbb3a14cfdd4283dd59cc
Samuele Landi <samuele@landi.ae>
2021-04-04 09:17:18 +0100

    updated for 4g/lte module

--- gps2url.py
+++ gps2url.py
@@ -24,7 +24,7 @@ import socket
 import time 
 import hashlib
 import sys, getopt
-
+import ssl
 
 ############################ CUSTOM DEFAULT PARAMETERS #########################
 endpoint="https://yourdomainname.org?lat=%LAT%&long=%LONG%&uid=%HOSTNAME%";
@@ -34,7 +34,16 @@ interval = 6.0	    # report the position every xx seconds
 intervalsameposition= 300.0  # report the same position every xxx seconds
 secretseed= "478c60b2f1e806f41f4b81d882594e8a54b9d2554dd1df30b9607024a7bb39ac"
 ################## END CUSTOM PARAMETERS #######################################
-
+# functiont to decode coordinates
+def decode(coord):
+    #Converts DDDMM.MMMMM -> DD deg MM.MMMMM min
+    x = coord.split(".")
+    head = x[0]
+    tail = x[1]
+    deg = head[0:-2]
+    min = head[-2:]
+    return deg + " deg " + min + "." + tail + " min"
+    
 # get command line parameters if any
 try:
    opts, args = getopt.getopt(sys.argv[1:],"he:w:r:i:s:x:",["endpoint=","portwrite=","portread","interval","intervalsamepos","secretseed"])
@@ -62,13 +71,29 @@ for opt, arg in opts:
 print("[Info] Connecting Serial Port..")
 try:
     serw = serial.Serial(portwrite, baudrate = 115200, timeout = 1,rtscts=True, dsrdtr=True)
-    serw.write('AT+QGPS=1\r'.encode())
-    serw.close()
+    # for https://sixfab.com/product/raspberry-pi-lte-m-nb-iot-egprs-cellular-hat/
+    print("Configuration: (1/5)")
+#    serw.write('AT+QGPS=1\r'.encode())
+#    time.sleep(1)
+    # for https://docs.sixfab.com/docs/raspberry-pi-3g-4g-lte-base-hat-introduction
+    print("Configuration: (2/5)")
+#    serw.write('AT$GPSRST\r'.encode())
+#    time.sleep(1)
+    print("Configuration: (3/5)")
+#    serw.write('AT$GPSNVRAM=15,0\r'.encode())
+#    time.sleep(1)
+    print("Configuration: (4/5)")
+#    serw.write('AT$GPSNMUN=2,1,1,1,1,1,1\r'.encode())
     time.sleep(1)
+#    serw.write('AT$GPSP=1\r'.encode())
+#    serw.close()
+    time.sleep(1)
+    print("Configuration: (5/5) completed")
 except Exception as e: 
     print("[Error] Serial port connection failed. Please check the custom parameters")
     print(e)
     quit()
+
 # initialize some variables
 newread = 0.0
 lastspeed="-1"
@@ -90,23 +115,27 @@ while True:
             print("[Warning] GPS location not yet available\n")
         #parse data    
         tm = sdata[1][0:2] + ":" + sdata[1][2:4] + ":" + sdata[1][4:6] #time
-        lat = sdata[3] 	     	#latitude
+        lat = decode(sdata[3]) 	     	#latitude
         dirLat = sdata[4]      	#latitude direction N/S
-        lon = sdata[5] 		#longitude
+        lon = decode(sdata[5]) 		#longitude
         dirLon = sdata[6]      	#longitude direction E/W
         speed = sdata[7]       	#speed in knots
         trueCourse = sdata[8]   #true course
         dt = sdata[9][0:2] + "-" + sdata[9][2:4] + "-" + sdata[9][4:6] #date
+        latitudev = lat.split()
+        longitudev = lon.split()
+        latitude=str(int(latitudev[0]) + (float(latitudev[2])/60))
+        longitude=str(int(longitudev[0]) + (float(longitudev[2])/60)) 
         # print data        
-        print("[Info] Date: %s time : %s, latitude : %s(%s), longitude : %s(%s), speed : %s,True Course : %s,"%   (dt,tm,lat,dirLat,lon,dirLon,speed,trueCourse))
+        print("[Info] Date: %s time : %s, latitude : %s(%s), longitude : %s(%s), speed : %s,True Course : %s,"%   (dt,tm,latitude,dirLat,longitude,dirLon,speed,trueCourse))
         # Signature computation by SHA3-256
-        tosign=lat+dirLat+lon+dirLon+socket.gethostname()+secretseed
+        tosign=latitude+dirLat+longitude+dirLon+socket.gethostname()+secretseed
         encoded_tosign = tosign.encode()
         bta = bytearray(encoded_tosign)
         signature=hashlib.sha3_256(bta).hexdigest()
         # replace variables
-        buf=endpoint.replace("%LAT%",lat+dirLat)
-        buf1=buf.replace("%LONG%",lon+dirLon)
+        buf=endpoint.replace("%LAT%",latitude+dirLat)
+        buf1=buf.replace("%LONG%",longitude+dirLon)
         buf=buf1.replace("%HOSTNAME%",socket.gethostname())
         buf1=buf.replace("%SPEED%",speed)
         buf=buf1.replace("%SIGNATURE%",signature)
@@ -119,8 +148,11 @@ while True:
             continue
         # Call endpoint URL
         print("[Info] Contacting endpoint: %s\n"% endpoints)
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
         try:     
-            with urllib.request.urlopen(endpoints) as response:
+            with urllib.request.urlopen(endpoints,context=ctx) as response:
                 answer = response.read()
         except Exception as e: 
             print("[Error] Endpoint not reachable with error:",e)
@@ -129,3 +161,4 @@ while True:
         newread=time.time()+interval
         lasttimeurl=time.time()
    time.sleep(0.1)
+   
